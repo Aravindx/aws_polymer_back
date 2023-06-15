ac7adb3d61c3803e0b1879882f95320fc8504307
Aravindx <aravindmathi517@gmail.com>
2023-06-15 23:52:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const projectmodel = require("./models/project");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://arav:QcEl0tg419vOS6rR@cluster0.k1dszd3.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.get("/getprojects", async (req, res) => {
+  const allProjects = await projectmodel.find();
+  res.status(200).send({ status: "Success", data: allProjects });
+});
+
+app.listen(9000, () => {
+  console.log("Server running on port 9000");
+});
